9bda8e282dd2688db587229e853e52d49b243756
sohof <dastmard@gmail.com>
2017-12-08 10:37:22 +0100

    Initial commit

---
+++ src/datalagring/JDBCLabb.java
@@ -0,0 +1,21 @@
+/*
+ * Datalagring 
+ * Ett liten program som ska testa java JDBC. Ska koppla mot en mysql databas,
+ * och köra några queries. 
+ */
+package datalagring;
+
+/**
+ *
+ * @author sohof
+ */
+public class JDBCLabb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
