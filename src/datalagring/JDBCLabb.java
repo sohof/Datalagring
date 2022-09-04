25fc96f67779f4e351b082617c76604f098f27c5
Sohof Dastmard <dastmard@gmail.com>
2022-09-04 10:20:24 +0200

    added comment to simpleselect

--- src/datalagring/JDBCLabb.java
+++ src/datalagring/JDBCLabb.java
@@ -1,7 +1,7 @@
 /*
  * Datalagring IV1351 A small example to show how JDBC works.
- * It uses the JDBC driver for mysql. 
- * There is no error management, instead exceptions are thrown. 
+ * It uses the JDBC driver for mysql.
+ * There is no error management, instead exceptions are thrown.
  */
 package datalagring;
 
@@ -21,28 +21,28 @@ public class JDBCLabb {
     // DB connection variables
     static protected Connection con;
     // DB access variables
-    private static final String URL = "jdbc:mysql:///labb";  // labb is the name of the database 
-    private static final String driver ="com.mysql.jdbc.Driver"; 
+    private static final String URL = "jdbc:mysql:///labb";  // labb is the name of the database
+    private static final String driver ="com.mysql.jdbc.Driver";
     private static final String userID ="root";
     private static final String passWord ="Mazdak123";
-    
+
     // method for establishing connection to the database
     public void connect(){
-    
+
     }
-    
+    // method for doing a simpleSelect
     public void simpleSelect() throws Exception {
-    
+
     }
-    
+
       public void parameterizedSelect() throws Exception {
-          
+
           // Local variables
           String query;
           ResultSet rs;
           PreparedStatement stmt;
           String brandparam;
-          
+
          // Create a Scanner in order to allow the user to provide input.
         Scanner in = new Scanner(System.in);
         // Ask use for input
@@ -50,17 +50,17 @@ public class JDBCLabb {
         // Save string input  into brandparam
         brandparam = in.nextLine();
     }
-    
+
     // Insert values into the database
     public void insert() throws Exception {
-        
+
         // Local variales;
         String query;
         PreparedStatement stmt;
         String fnameparam;
         String lnameparam;
         String cityparam;
-        
+
         // Create a Scanner in order to allow the user to provide input.
         Scanner in = new Scanner(System.in);
         // Ask user to specify first name for input
@@ -71,22 +71,22 @@ public class JDBCLabb {
         System.out.print("Please enter last name: ");
         // Save string input into lnameparam
         lnameparam = in.nextLine();
-        // Ask user to specify city 
+        // Ask user to specify city
         System.out.print("Please enter city name: ");
         // Save string input into cityparam
         cityparam = in.nextLine();
-        
-    }  
-      
-      
+
+    }
+
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        
+
         // create a new of this class
-        JDBCLabb t = new JDBCLabb(); 
-        
+        JDBCLabb t = new JDBCLabb();
+
         // Call methods on the object t.
 	System.out.println("-------- connect() ---------");
         t.connect();
@@ -101,7 +101,7 @@ public class JDBCLabb {
         con.commit();
         // Close the connection.
         con.close();
-        
+
     }
-    
+
 }
